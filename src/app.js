411ae1b0b3b5245face3087aad30fc9487a637e0
Jamie Reid <wjreid85.jr@gmail.com>
2019-01-16 13:24:54 +0000

    Sets up initial files. Requests api data. Publishes api data.

---
+++ src/app.js
@@ -0,0 +1,10 @@
+const Countries = require('./models/countries.js');
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('JavaScript Loaded');
+
+  const countries = new Countries();
+  countries.getData();
+
+});
---
+++ src/models/countries.js
@@ -0,0 +1,19 @@
+const RequestHelper = require('../helpers/request_helper.js');
+const PubSub = require('../helpers/pub_sub.js');
+
+
+const Countries = function() {
+  this.text = null;
+};
+
+
+Countries.prototype.getData = function () {
+  const request = new RequestHelper('https://restcountries.eu/rest/v2/all');
+  request.get((data) => {
+    this.text = data;
+    console.dir(this.text);
+    PubSub.publish('Countries:allCountriesData', this.text);
+  });
+};
+
+module.exports = Countries;
